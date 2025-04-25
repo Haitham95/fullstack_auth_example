912795af6a9adad77d6f71956e49da5c5b517566
haitham <haithamsm95@gmail.com>
2025-04-25 04:56:00 +0300

    applying the userModule inside of the app, tested by postman

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
@@ -14,6 +15,7 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
       }),
       inject: [ConfigService],
     }),
+    UsersModule,
   ],
   controllers: [],
   providers: [],
