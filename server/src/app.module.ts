5ebdd71d1a077c6747a7d77ac79bcafa5b303690
haitham <haithamsm95@gmail.com>
2025-04-26 21:40:34 +0300

    applying auth module through the app after testing it by postman

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,12 +2,15 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
+// TODO: Loggers and throttling
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+
     MongooseModule.forRootAsync({
       imports: [ConfigModule],
       useFactory: async (configService: ConfigService) => ({
@@ -16,6 +19,7 @@ import { UsersModule } from './users/users.module';
       inject: [ConfigService],
     }),
     UsersModule,
+    AuthModule,
   ],
   controllers: [],
   providers: [],
