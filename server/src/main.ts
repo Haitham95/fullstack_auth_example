2d98aca0f098dada786a3ebc0a411d155738512c
haitham <haithamsm95@gmail.com>
2025-04-27 04:23:00 +0300

    installing helmet for headers, and applying CORS

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,12 +3,27 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import * as cookieParser from 'cookie-parser';
 
+import helmet from 'helmet';
+// somewhere in your initialization file
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.use(cookieParser());
-  await app.listen(process.env.PORT ?? 3001);
+  app.use(
+    helmet({
+      contentSecurityPolicy: false,
+    }),
+  );
 
   // TODO: Apply CORS for security
+  app.enableCors({
+    origin: ['http://localhost:5173'],
+    credentials: true,
+    methods: ['GET', 'POST', 'PATCH', 'DELETE'], // allowed methods
+    allowedHeaders: ['Content-Type', 'Authorization', 'Cookie'], // allowed headers
+  });
+
+  await app.listen(3001);
 }
 bootstrap();
